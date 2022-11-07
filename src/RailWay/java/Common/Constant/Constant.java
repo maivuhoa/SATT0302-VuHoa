e2fef6d1308c9d46ee4e231a538d4090bcecceda
SATTDN22.03.02 Mai Vu Hoa <SATTDN22.03.02@logigear.com>
2022-11-07 17:12:11 +0700

    update

---
+++ src/RailWay/java/Common/Constant/Constant.java
@@ -0,0 +1,8 @@
+package Common.Constant;
+import org.openqa.selenium.WebDriver;
+public class Constant {
+    public static WebDriver WEBDRIVER;
+    public static final String RailwayURL="http://www.railwayb2.somee.com/Page/HomePage.cshtml";
+    public static final String Username="aa@gmail.com";
+    public static final String Password="12345678";
+}
---
+++ src/RailWay/java/PageObjects/LoginPage.java
@@ -0,0 +1,54 @@
+package PageObjects;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+import Common.Constant.Constant;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class LoginPage extends GeneralPage{
+    // Locator
+ private final By userName=By.xpath("//input[@id='username']");
+ private final  By passWord=By.xpath("//input[@id='password']");
+ private final By btnLogin=By.xpath("//input[@value='Login']");
+ private final By txtLoginErrorMes=By.xpath("//p[@class='message error LoginForm']");
+
+ private final By stWelcomeText = By.xpath("//div[@class='account']/strong");
+
+ public WebElement getWellComeMes(){
+     return getTxtWellComeMes();
+ }
+public WebElement getTxtUserName(){
+   return Constant.WEBDRIVER.findElement(userName);
+}
+public WebElement getTxtPassWord(){
+    return Constant.WEBDRIVER.findElement(passWord);
+}
+public WebElement getBtnLogin(){
+    return Constant.WEBDRIVER.findElement(btnLogin);
+}
+
+public WebElement getTabLogin(){
+     return super.getTabLogin();
+}
+
+public WebElement getTxtMessError(){
+    return  Constant.WEBDRIVER.findElement(txtLoginErrorMes);
+}
+    //  Methods
+public void testLogin(String username,String PassWord){
+    getTxtUserName().sendKeys(username);
+    getTxtPassWord().sendKeys(PassWord);
+    getBtnLogin().click();
+
+}
+
+
+}
